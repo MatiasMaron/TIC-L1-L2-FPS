90370fb9ef1cc0b4173cdd23ea2e10034b629428
MatiasMaron <101577603+MatiasMaron@users.noreply.github.com>
2022-08-05 11:01:57 -0300

    Ejercicio 4

---
+++ Assets/Scripts/Netbooks.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Netbooks : MonoBehaviour
+{
+    public GameObject Netbook;
+ 
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Carrito"))
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                Instantiate(Netbook);
+            }
+        }
+     
+    }
+}
