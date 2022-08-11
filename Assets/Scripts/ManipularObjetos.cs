db51055bfed13daba0042b9c018049d6ecb2c868
MatiasMaron <101577603+MatiasMaron@users.noreply.github.com>
2022-08-11 09:07:37 -0300

    Ejercicios clase 2 arrays

---
+++ Assets/Scripts/ManipularObjetos.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipularObjetos : MonoBehaviour
+{
+    [SerializeField]GameObject[] sillas;
+    
+    void Start()
+    {
+        sillas = GameObject.FindGameObjectsWithTag("Silla");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            //DesactivarElementosDelArray();
+            AgregarRBAElementosDelArray();
+            AgregarBCAElementosDelArray();
+        }
+    }
+
+    void DesactivarElementosDelArray()
+    {
+        for(int i = 0;i<sillas.Length;i++)
+        {
+            sillas[i].SetActive(false);
+        }
+    }
+
+    void AgregarRBAElementosDelArray()
+    {
+        for (int i = 0;i<sillas.Length;i++)
+        {
+            sillas[i].AddComponent<Rigidbody>();
+        }
+    }
+
+    void AgregarBCAElementosDelArray()
+    {
+        for (int i = 0; i < sillas.Length; i++)
+        {
+            sillas[i].AddComponent<BoxCollider>();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/NumerosAlAzar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumerosAlAzar : MonoBehaviour
+{
+    [SerializeField]int randomNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            GenerarNumeroAleatorio();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerarNumeroAleatorio()
+    {
+        randomNumber = Random.Range(0, 4);
+    }
+}
